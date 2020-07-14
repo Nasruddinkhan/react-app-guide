c8af42f90d894a6fbe839636bdd7555177a30058
Nasruddinkhan <nasruddinkhan44@gmail.com>
2020-07-14 16:50:43 +0530

    add functiona component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Employee from './Employee/Employee'
 function App() {
-  /*return (
+  return (
     <div className="App">
       <h1>Hi Nasruddin, <br/> This is your first react App </h1>
+      <p>It's is paragraph</p>
+      <Employee/>
     </div>
-  );*/
-  return React.createElement('div', {className : 'App'},  React.createElement('h1', null, 'Hi', '\ Nasruddin, <br/> This is your first react App '));
+  );
+//  return React.createElement('div', {className : 'App'},  React.createElement('h1', null, 'Hi', '\ Nasruddin, <br/> This is your first react App '));
 }
 
 export default App;
---
+++ src/Employee/Employee.js
@@ -0,0 +1,6 @@
+import React from 'react'
+const employee = () => {
+    return <p>I'm a Employee</p>
+}
+
+ export  default employee;
